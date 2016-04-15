1d423e23a21916b88d53e791c4725ffca4465f8b
Cody Johnson <cody@anpac.net>
2016-04-15 00:44:07 -0600

    Recruit View First Test

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,9 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('ionic-todo', ['ionic', 'LocalStorageModule','chart.js'])
 
-.run(function($ionicPlatform) {
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -21,4 +21,98 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.config(function (localStorageServiceProvider,$stateProvider, $urlRouterProvider) {
+    localStorageServiceProvider
+      .setPrefix('ionic-todo');
+      $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'templates/t2.html',
+                controller: 'main'
+            })
+            .state('users', {
+                url: '/users',
+                templateUrl: 'templates/template1.html',
+                controller: 'main'
+            })
+            .state('user', {
+                url: "/users/:userId",
+                templateUrl: "templates/user.html",
+                controller: "main"
+            });
+        $urlRouterProvider.otherwise('/users');
+  });
+  
+app.controller('main', function ($scope, $ionicModal, localStorageService,$http) {
+    //store the entities name in a variable
+    $http.get('http://johnsonfinancialservice.com/Test/getProspectsLiscenced.php').success(function(data) {
+
+		$scope.usersLiscenced = data;
+		localStorageService.set(taskData, data);
+		$scope.MaxIDLiscenced = Math.max.apply(Math, data.map(function(o) {
+			return o.ID;
+		}));
+	});
+
+    var taskData = 'task';
+
+    //initialize the tasks scope with empty array
+    $scope.tasks = [];
+
+    //initialize the task scope with empty object
+    $scope.task = [];
+
+    //configure the ionic modal before use
+    $ionicModal.fromTemplateUrl('new-task-modal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.newTaskModal = modal;
+    });
+
+    $scope.getTasks = function () {
+        //fetches task from local storage
+        if (localStorageService.get(taskData)) {
+            $scope.tasks = localStorageService.get(taskData);
+        } else {
+            $scope.tasks = [];
+        }
+    };
+
+    $scope.createTask = function () {
+        //creates a new task
+        $scope.tasks.push($scope.task);
+        localStorageService.set(taskData, $scope.tasks);
+        $scope.task = {};
+
+        //close new task modal
+        $scope.newTaskModal.hide();
+    };
+
+    $scope.removeTask = function (index) {
+        //removes a task
+        $scope.tasks.splice(index, 1);
+        localStorageService.set(taskData, $scope.tasks);
+    };
+
+
+    $scope.completeTask = function () {
+        //updates a task as completed
+        if (index !== -1) {
+            $scope.tasks[index].completed = true;
+        }
+
+        localStorageService.set(taskData, $scope.tasks);
+
+    };
+
+    $scope.openTaskModal = function () {
+        $scope.newTaskModal.show();
+    };
+
+    $scope.closeTaskModal = function () {
+        $scope.newTaskModal.hide();
+    };
+});
